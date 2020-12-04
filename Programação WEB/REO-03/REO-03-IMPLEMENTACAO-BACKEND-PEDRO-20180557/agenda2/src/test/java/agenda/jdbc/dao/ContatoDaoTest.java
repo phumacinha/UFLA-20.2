24aaa8aa33252d8eb75c5419f061d71b897cc098
Pedro Antônio de Souza <pedro.souza2@estudante.ufla.br>
2020-12-04 20:04:54 -0300

    Ajuste de dependências

---
+++ Programação WEB/REO-03/REO-03-IMPLEMENTACAO-BACKEND-PEDRO-20180557/agenda2/src/test/java/agenda/jdbc/dao/ContatoDaoTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda.jdbc.dao;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pedro Antônio de Souza
+ */
+public class ContatoDaoTest {
+    
+    @Test
+    public void testAdiciona () {
+        ContatoDao dao = new ContatoDao();
+    }
+    
+}
