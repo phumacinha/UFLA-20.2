d622e727318a81e93daa7127c1edd80a54358517
Pedro <pedro.souza2@estudante.ufla.br>
2021-03-27 13:06:31 -0300

    Remoção de dados do BD local

--- Sistemas Distribuídos/REO-04/todolist/src/main/java/br/dcc/ufla/todolist/ToDoListApplication.java
+++ Sistemas Distribuídos/REO-04/todolist/src/main/java/br/dcc/ufla/todolist/ToDoListApplication.java
@@ -1,5 +1,7 @@
 package br.dcc.ufla.todolist;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -24,4 +26,9 @@ public class ToDoListApplication {
                 }
             };
 	}
+        
+        @PostConstruct
+        void started() {
+            TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        }
 }
